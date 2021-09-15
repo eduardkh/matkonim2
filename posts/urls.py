5ab396ff6b2f197410636e3711cd44f5e1daed6b
Eduard Khiaev <eddietheone1984@gmail.com>
2021-09-15 16:33:20 +0000

    image in index list

--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 import posts.views
+# for media files in test env
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 app_name = 'posts'
@@ -8,3 +11,6 @@ urlpatterns = [
     path('<slug:slug>', posts.views.detail, name='recipe_detail'),
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
