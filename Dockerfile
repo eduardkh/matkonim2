897ee833422921da715e159a34e25aefdf2bfa91
Eduard Khiaev <eddietheone1984@gmail.com>
2021-09-19 17:47:04 +0000

    add pg-db

--- Dockerfile
+++ Dockerfile
@@ -9,5 +9,4 @@ ADD . .
 # install dependencies
 RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
-# ENTRYPOINT [ "python3" ]
-CMD [ "python3", "manage.py", "runserver", "0:8000"]
+CMD [ "python", "manage.py", "runserver", "0.0.0.0:8000"]
--- matkonim2/settings.py
+++ matkonim2/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = 'matkonim2.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
 
@@ -125,7 +129,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/uploads/'
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_ROOT = BASE_DIR / 'uploads'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles"),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles"), ]
 # python manage.py findstatic --verbosity 2 style.css (tshoot)
 
 
